9507ba5fb9930ec730fedba7ac600076a2b32438
[poww10s] <[theironminer@gmail.com]>
2018-11-08 05:16:37 -0500

    responsive grid and menu changes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+//const menuBranding = document.querySelector('.menu-branding');
+const main = document.querySelector('#home');
+const title = document.querySelector('#title');
+const footer = document.querySelector('#main-footer');
 const navItems = document.querySelectorAll('.nav-item');
 
 //set initial state of menu
@@ -16,7 +19,10 @@ function toggleMenu() {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+        //menuBranding.classList.add('show');
+        main.classList.add('show');
+        title.classList.add('show');
+        footer.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
 
         // Set Menu State
@@ -25,7 +31,10 @@ function toggleMenu() {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
+        //menuBranding.classList.remove('show');
+        main.classList.remove('show');
+        title.classList.remove('show');
+        footer.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
 
         // Set Menu State
